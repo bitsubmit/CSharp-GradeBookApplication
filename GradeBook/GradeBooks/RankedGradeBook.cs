e0d0ccdb209be7596ea4a8b038443561c3fa4e83
Simon Jakobsen <simonjakobsen9@hotmail.com>
2021-01-13 10:55:56 +0100

    Completed module

---
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -0,0 +1,12 @@
+﻿using GradeBook.Enums;
+
+namespace GradeBook.GradeBooks
+{
+    public class RankedGradeBook : BaseGradeBook
+    {
+        RankedGradeBook(string name) : base(name)
+        {
+            this.Type = GradeBookType.Ranked;
+        }
+    }
+}
---
+++ GradeBook/GradeBooks/StandardGradebook.cs
@@ -0,0 +1,12 @@
+﻿using GradeBook.Enums;
+
+namespace GradeBook.GradeBooks
+{
+    public class StandardGradebook : BaseGradeBook
+    {
+        StandardGradebook(string name) : base(name)
+        {
+            this.Type = GradeBookType.Standard;
+        }
+    }
+}
